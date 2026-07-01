b4b7d39e7ac09fa4a384681b361ad341aa064ed5
Developer-Aisurya <aisuryamallick1@outlook.com>
2026-07-02 00:32:52 +0530

    Delete FitZone Gym directory

--- FitZone Gym/js/script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    header.style.background = "rgba(0,0,0,.85)";
-  } else {
-    header.style.background = "rgba(0,0,0,.3)";
-  }
-});
